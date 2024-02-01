dd4ade4627161f7752500bef38d088690a5b493e
GermanBalaguera954 <germanbalaguera954@gmail.com>
2024-02-01 13:30:59 -0500

    Globalizacion

--- TareasMVC/obj/Debug/net7.0/TareasMVC.AssemblyInfo.cs
+++ TareasMVC/obj/Debug/net7.0/TareasMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab60d0b643dcf7f35bd2a5ef3560eaf93f2e1fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ae1fa36bfa492832a15c9c23ccb678362ba54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
